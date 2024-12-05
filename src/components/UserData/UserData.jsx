3b73c69472ca0021ee06e010acdc56bcbc9fb98b
John Arocha <john.arocha@clover.com>
2024-12-05 11:47:21 -0700

    finished card styling for awhile. next I need to style the top, icon, title, card spacing, and make a login page

--- src/components/UserData/UserData.jsx
+++ src/components/UserData/UserData.jsx
@@ -7,25 +7,25 @@ export default function UserData({ secureData }) {
         <Container className="w-100 d-flex flex-column justify-content-between align-items-center text-white">
             <Row className="mb-4">
                 <Col xs={12} className="d-flex justify-content-center">
-                    <p>{secureData.website}</p>
+                    <p className="field" >{secureData.website}</p>
                 </Col>
             </Row>
             
             <Row className="mb-4">
                 <Col xs={6} className="text-start">
-                    <p>Username:</p>
+                    <p className="field" >Username:</p>
                 </Col>
                 {/* Make this a component that is a rounded input that hides the data, will be used for both user and pass and will have a copy button on the right hand side for ease of use */}
                 <Col xs={6} className="text-end">
-                    <input type="text" value={secureData.email} readOnly className="form-control dark-input" />
+                    <input type="text" value={secureData.email} readOnly className="form-control dark-input field" />
                 </Col>
             </Row>
             <Row className="mb-4">
                 <Col xs={6}>
-                    <p>Password:</p>
+                    <p className="field" >Password:</p>
                 </Col>
                 <Col xs={6}>
-                    <input type="password" value={secureData.password} readOnly className="form-control dark-input" />
+                    <input type="password" value={secureData.password} readOnly className="form-control dark-input field" />
                 </Col>
             </Row>
             <Row className="w-100 d-flex justify-content-between align-items-center">
