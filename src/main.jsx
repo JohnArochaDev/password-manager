23c736a911101b97e09195a99c77aa08e70b49af
John Arocha <john.arocha@clover.com>
2024-12-10 14:38:21 -0700

    added conditional rendering for a login page

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react';
+import { StrictMode, useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Col, Row, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap CSS is imported
@@ -8,22 +8,45 @@ import TempApp from './components/TempApp/TempApp.jsx'; // temporary for styling
 import App from './components/App/App.jsx';
 import { Container } from 'react-bootstrap';
 
-createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <div className="custom-bg">
-            <Container fluid className="container-fullwidth d-flex flex-column justify-content-center align-items-center text-white">
-                <Row className="w-100 d-flex justify-content-between align-items-center">
-                    <Col xs="auto">
-                        <img src="shield-lock-line-icon.png" alt="icon" className="m-3" />
-						{/* here is the link for later : https://uxwing.com/shield-lock-line-icon/ */}
-                    </Col>
-                    <Col xs="auto">
-						<Button className="m-3" variant="danger" style={{ backgroundColor: '#344955', borderColor: '#50727B' }}>...</Button>
-                    </Col>
-                </Row>
-                {/* <TempApp /> */}
-                <App />
-            </Container>
-        </div>
-    </StrictMode>
-);
+
+
+export default function Main() {
+
+    const [loggedin, setLoggedin] = useState(false)
+
+
+    useEffect(() => {
+        const token = localStorage.getItem('jwtToken');
+        if (token) {
+            setLoggedin(true)
+        }
+    }, [])
+
+    return(
+        <StrictMode>
+            <div className="custom-bg">
+                <Container fluid className="container-fullwidth d-flex flex-column justify-content-center align-items-center text-white">
+                    <Row className="w-100 d-flex justify-content-between align-items-center">
+                        <Col xs="auto">
+                            <img src="shield-lock-line-icon.png" alt="icon" className="m-3" />
+                            {/* here is the link for later : https://uxwing.com/shield-lock-line-icon/ */}
+                        </Col>
+                        <Col xs="auto">
+                            <Button className="m-3" variant="danger" style={{ backgroundColor: '#344955', borderColor: '#50727B' }}>...</Button>
+                        </Col>
+                    </Row>
+
+
+                    <App />
+                    {loggedin ? <App /> : <Login />}
+
+
+
+                </Container>
+            </div>
+        </StrictMode>
+    );
+}
+
+
+createRoot(document.getElementById('root')).render(<Main />);
