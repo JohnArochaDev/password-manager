61eca42b6cee38b0b2ee919163d7766636f8d79d
John Arocha <john.arocha@clover.com>
2024-12-11 10:19:28 -0700

    added dropdown menu with styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode, useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Col, Row, Button, Container } from 'react-bootstrap';
+import { Col, Row, Container, Dropdown } from 'react-bootstrap';
 
 import App from './components/App/App.jsx';
 import Login from './components/Login/Login.jsx';
@@ -9,10 +9,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap CSS is import
 import './main.css'; // Import custom CSS
 
 export default function Main() {
-
-    const [loggedin, setLoggedin] = useState(false)
-    const [reload, setReload] = useState(false)
-
+    const [loggedin, setLoggedin] = useState(false);
+    const [reload, setReload] = useState(false);
 
     useEffect(() => {
         const token = localStorage.getItem('jwtToken');
@@ -33,7 +31,7 @@ export default function Main() {
         }
     }, [reload]);
 
-    return(
+    return (
         <StrictMode>
             <div className="custom-bg">
                 <Container fluid className="container-fullwidth d-flex flex-column justify-content-center align-items-center text-white">
@@ -43,21 +41,25 @@ export default function Main() {
                             {/* here is the link for later : https://uxwing.com/shield-lock-line-icon/ */}
                         </Col>
                         <Col xs="auto">
-                            <Button className="m-3 custom-button" >...</Button>
+                            <Dropdown>
+                                <Dropdown.Toggle className="m-3 custom-button">
+                                    ...
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu className="custom-dropdown-menu">
+                                    <Dropdown.Item href="#/action-1">Action 1</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-2">Action 2</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-3">Action 3</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
                         </Col>
                     </Row>
 
-
-                    {/* <App /> */}
                     {loggedin ? <App /> : <Login reload={reload} setReload={setReload} />}
-
-
-
                 </Container>
             </div>
         </StrictMode>
     );
 }
-
 
 createRoot(document.getElementById('root')).render(<Main />);
