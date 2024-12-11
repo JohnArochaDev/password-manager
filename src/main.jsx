67601e09fbe1d0c0c4f9e2c6f9716b93339c5c10
John Arocha <john.arocha@clover.com>
2024-12-11 10:21:02 -0700

    added dropdown menu items

--- src/main.jsx
+++ src/main.jsx
@@ -47,9 +47,8 @@ export default function Main() {
                                 </Dropdown.Toggle>
 
                                 <Dropdown.Menu className="custom-dropdown-menu">
-                                    <Dropdown.Item href="#/action-1">Action 1</Dropdown.Item>
-                                    <Dropdown.Item href="#/action-2">Action 2</Dropdown.Item>
-                                    <Dropdown.Item href="#/action-3">Action 3</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-1">Settings</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-3">Logout</Dropdown.Item>
                                 </Dropdown.Menu>
                             </Dropdown>
                         </Col>
